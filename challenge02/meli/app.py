5e80483bdc1ebd755658ccd92cb706a5511e99f3
ceguzman <ceguzman582@misena.edu.co>
2025-06-05 17:58:38 -0500

    update: get_valid_token service

--- challenge02/meli/app.py
+++ challenge02/meli/app.py
@@ -1,30 +1,13 @@
-import os
-
-from meli.auth.oauth import OuathApi
-from meli.auth.config.constants import secrets
-from meli.utils.env_writer import update_env
+from meli.services.oauth_service import get_valid_access_token
 
 
 def main():
-    oauth_api = OuathApi(secrets=secrets)
-
-    refresh_token = os.getenv("REFRESH_TOKEN")
-
-    if refresh_token:
-        print("🔁 Using stored refresh_token...")
-        tokens = oauth_api.refresh_access_token(refresh_token)
-    else:
-        print("🔐 Using authorization code to get tokens...")
-        tokens = oauth_api.get_access_token()
-
-        if "refresh_token" in tokens:
-            update_env("REFRESH_TOKEN", tokens["refresh_token"])
-            print("📝 refresh_token saved to .env")
+    tokens = get_valid_access_token()
 
     if "access_token" in tokens:
-        print("✅ Access token:", tokens["access_token"])
+        print("Access token:", tokens["access_token"])
     else:
-        print("❌ Error getting access token:", tokens)
+        print("Error getting access token:", tokens)
 
 
 if __name__ == "__main__":
---
+++ challenge02/meli/services/oauth_service.py
@@ -0,0 +1,24 @@
+import os
+from meli.auth.oauth import OuathApi
+from meli.auth.config.constants import secrets
+from meli.utils.env_writer import update_env
+
+
+def get_valid_access_token():
+    oauth_api = OuathApi(secrets=secrets)
+
+    refresh_token = os.getenv("REFRESH_TOKEN")
+    tokens = None
+
+    if refresh_token:
+        print("Using stored refresh_token...")
+        tokens = oauth_api.refresh_access_token(refresh_token)
+    else:
+        print("Using authorization code to get tokens...")
+        tokens = oauth_api.get_access_token()
+
+        if "refresh_token" in tokens:
+            update_env("REFRESH_TOKEN", tokens["refresh_token"])
+            print("refresh_token saved to .env")
+
+    return tokens
