7efc524d3f0d60a168cfcf05bee54d3cf1abb846
ceguzman <ceguzman582@misena.edu.co>
2025-06-05 17:50:58 -0500

    update: add refresh token

--- challenge02/meli/app.py
+++ challenge02/meli/app.py
@@ -1,29 +1,30 @@
+import os
+
 from meli.auth.oauth import OuathApi
 from meli.auth.config.constants import secrets
+from meli.utils.env_writer import update_env
 
 
 def main():
     oauth_api = OuathApi(secrets=secrets)
 
-    # Primer flujo con auth code (solo una vez)
-    tokens = oauth_api.get_access_token()
+    refresh_token = os.getenv("REFRESH_TOKEN")
+
+    if refresh_token:
+        print("🔁 Using stored refresh_token...")
+        tokens = oauth_api.refresh_access_token(refresh_token)
+    else:
+        print("🔐 Using authorization code to get tokens...")
+        tokens = oauth_api.get_access_token()
+
+        if "refresh_token" in tokens:
+            update_env("REFRESH_TOKEN", tokens["refresh_token"])
+            print("📝 refresh_token saved to .env")
 
     if "access_token" in tokens:
         print("✅ Access token:", tokens["access_token"])
-        print("🔁 Refresh token:", tokens["refresh_token"])
-
-        # Simulamos renovación usando refresh_token
-        refreshed = oauth_api.refresh_access_token(tokens["refresh_token"])
-
-        if "access_token" in refreshed:
-            print("\n🔄 Token refreshed:")
-            print("✅ New access token:", refreshed["access_token"])
-            print("🆕 New refresh token:", refreshed["refresh_token"])
-        else:
-            print("❌ Error refreshing token:", refreshed)
-
     else:
-        print("❌ Error obtaining initial tokens:", tokens)
+        print("❌ Error getting access token:", tokens)
 
 
 if __name__ == "__main__":
